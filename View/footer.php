ec75e63b79eeda0595c99129c3b38476b7f513ab
Matheus Vicente <50172954+matheus-vicente@users.noreply.github.com>
2020-12-09 21:22:07 -0300

    Melhorando footer

--- View/footer.php
+++ View/footer.php
@@ -1,70 +1,97 @@
 <style>
-    footer {
-        position: relative;
-        width: 100%;
-        height: 400px;
-        background-color: rgba(35, 114, 199, 0.8);
-        color: #fff;
-    }
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+	}
 
-        footer #headFooter {
-            padding: 50px 50px 20px 50px;
-        }
+	a {
+		text-decoration: none;
+		color: #000
+	}
 
-        footer p {
-            text-align: justify;
-            column-count: 2;
-        }
+	ul {
+		list-style: none;
+	}
+		
+	footer {
+		margin-top: 1rem;
+		position: relative;
+		width: 100%;
+		background-color: rgba(35, 114, 199, 0.8);
+		color: #fff;
+	}
 
+	#headFooter {
+		padding-bottom: 1rem;
+	}
 
-        #dvCopy {
-            display: flex;
-            align-items: center;
-            width: 100%;
-            height: 35px;
-            padding-left: 15px;
-            background-color: rgba(35, 114, 199, 1);
-        }
+	.title {
+		background: #175fac;
+		padding: 1rem;
+	}
 
+	.contentFooter {
+		display: flex;
+		padding: 1rem;
+	}
 
+	.candidatos {
+		margin-right: 1rem;
+	}
+	
+	.contentTitle {
+		margin-bottom: 0.4rem;
+	}
+
+	.contentList li {
+		margin-bottom: 0.2rem;
+	}
+
+	#dvCopy {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 35px;
+		padding-left: 15px;
+		background-color: rgba(35, 114, 199, 1);
+	}
 </style>
 
 <footer>
-        <div id="headFooter">
-            <h2>Catho</h2>
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-        </div>
+	<div id="headFooter">
+		<div class="title">
+			<h2>Vagas Online</h2>
+		</div>
 
-        <div id="dvCopy">
-            <p id="copy">© <?= date("Y"); ?> Copyright Todos direitos reservados.</p>
-        </div>
-    </footer>
+		<div class="contentFooter">
+			<div class="candidatos">
+				<h3 class="contentTitle">Para candidatos</h3>
+
+				<ul class="contentList">
+					<li><a href="#">Buscar vagas</a></li>
+					<li><a href="#">Por área</a></li>
+					<li><a href="#">Por cidade</a></li>
+					<li><a href="#">Currículo</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</div>
+
+			<div class="empresas">
+				<h3 class="contentTitle">Para Empresas</h3>
+
+				<ul class="contentList">
+					<li><a href="#">Cadastrar vagas</a></li>
+					<li><a href="#">Visualizar vagas</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div id="dvCopy">
+		<p id="copy">© <?= date("Y"); ?> Copyright Todos direitos reservados.</p>
+	</div>
+</footer>
