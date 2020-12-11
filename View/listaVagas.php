2febf84fb0a42e7779689ff59cce7f80abb2cb22
Matheus Vicente <50172954+matheus-vicente@users.noreply.github.com>
2020-12-11 19:48:21 -0300

    Adicionando tela lista de vagas

---
+++ View/listaVagas.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/listaVagas.css">
+  </head>
+
+  <body>
+
+    <?php include "header.php"; ?>
+    
+    <main id="listaVagas">
+      <div class="container">
+        <div class="textTitle">
+          <h1>Lista de vagas</h1>
+        </div>
+
+        <div class="content">
+          <div class="vaga">
+            <div class="dadosVaga">
+              <h2 class="nomeVaga">Empresa</h2>
+              <p class="descricaoVaga">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+
+            <a class="linkVaga" href="http://localhost/VagasOnline/view/vaga.php">Conhecer Empresa</a>
+          </div>
+          
+          <div class="vaga">
+            <div class="dadosVaga">
+              <h2 class="nomeVaga">Empresa</h2>
+              <p class="descricaoVaga">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+
+            <a class="linkVaga" href="http://localhost/VagasOnline/view/vaga.php">Conhecer Empresa</a>
+          </div>
+
+          <div class="vaga">
+            <div class="dadosVaga">
+              <h2 class="nomeVaga">Empresa</h2>
+              <p class="descricaoVaga">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+
+            <a class="linkVaga" href="http://localhost/VagasOnline/view/vaga.php">Conhecer Empresa</a>
+          </div>
+
+          <div class="vaga">
+            <div class="dadosVaga">
+              <h2 class="nomeVaga">Empresa</h2>
+              <p class="descricaoVaga">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+
+            <a class="linkVaga" href="http://localhost/VagasOnline/view/vaga.php">Conhecer Empresa</a>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <?php include "footer.php"; ?>
+
+  </body>
+</html>
